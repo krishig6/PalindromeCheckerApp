02e3f6024b2824f50bb077328866c267cc8ccbe5
Samir-dhakal <samirdhakal.9279@gmail.com>
2026-03-10 14:46:25 +0530

    UC5StackBasedPalindromeChecker

--- PalindromeCheckerApp1/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp1/src/PalindromeCheckerApp.java
@@ -1,18 +1,35 @@
-public class UseCase3PalindromeCheckerApp {
+import java.util.Stack;
+import java.util.Scanner;
+public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
 
-        String input = "madam";
-        String reversed = "";
 
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            Stack<Character> stack = new Stack<>();
+
+            System.out.print("Enter a string: ");
+            String str = sc.nextLine();
+
+            // Push characters into stack
+            for (int i = 0; i < str.length(); i++) {
+                stack.push(str.charAt(i));
+            }
+
+            // Pop characters to create reversed string
+            String reversed = "";
+            while (!stack.isEmpty()) {
+                reversed = reversed + stack.pop();
+            }
+
+            // Check palindrome
+            if (str.equals(reversed)) {
+                System.out.println("Palindrome");
+            } else {
+                System.out.println("Not a Palindrome");
+            }
+
+            sc.close();
         }
 
-        if (input.equals(reversed)) {
-            System.out.println(input + " is a palindrome");
-        } else {
-            System.out.println(input + " is not a palindrome");
-        }
-    }
 }
